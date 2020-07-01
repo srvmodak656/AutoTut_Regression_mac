c63415ef071d93d1ea4a179978af57bd176962dc
souravmodak <souM#@583>
2020-07-01 15:59:26 +0530

    Introducing JYTHON (java + python) module in Regression

--- Java/src/Regression.java
+++ Java/src/Regression.java
@@ -1,6 +1,19 @@
-
+/*
+ * This is the java module to run regression and is a multithreaded module.
+ * It is advised to be cautious about the code push since if now monitored 
+ * properly then the code which is pushed can crash the process management of 
+ * the operating system and can crash it. Also there is a possibility that 
+ * a not code which is written casually can mess with the other program which 
+ * is running in java jvm. So it is mandated to check it thoroughly with the 
+ * architect or author of this module before pushing it to the build.
+ * 
+ * AUTHOR :- Sourav Modak
+ * Date :- 1st July 2020
+ * INDIA
+ * 
+ */
 public class Regression {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
