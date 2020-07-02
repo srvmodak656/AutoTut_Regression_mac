32b5881481a881bc3377df71cd570ad531dbee91
souravmodak <souM#@583>
2020-07-02 22:05:45 +0530

    Some connections are given like file chooser [Incomplete code]

--- Java/src/RegressionGUI.java
+++ Java/src/RegressionGUI.java
@@ -4,24 +4,34 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileFilter;
+
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JTabbedPane;
 import java.awt.Panel;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegressionGUI {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JLabel timeoutLbl;
-	private JLabel workLbl;
-	private JLabel lblReportpath;
+	private static JFrame frame;
+	private static JTextField targetPathField;
+	private static JTextField workspaceField;
+	private static JTextField timeoutField;
+	private static JTextField reportPathField;
+	private static JLabel timeoutLbl;
+	private static JLabel workLbl;
+	private static JLabel lblReportpath;
+	public static String tempFilePath = "Imp/temp.txt";
 
 	/**
 	 * Launch the application.
@@ -49,6 +59,148 @@ public class RegressionGUI {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	
+	public static String getPresetDirectoryPath(String Path)
+	{
+		String path = null;
+		File file = new File(Path);
+		if(!file.exists())
+		{
+			try
+			{
+				file.createNewFile();
+			}
+			catch(Exception e1)
+			{
+				System.out.println("New File creation Error\n"+e1.getMessage());
+			}
+		}
+		try
+		{
+			ObjectInputStream objIn = new ObjectInputStream(new FileInputStream(file));
+			path = (String)objIn.readObject();
+			objIn.close();
+			return path;
+		}
+		catch(Exception e1)
+		{
+			System.out.println(e1.getLocalizedMessage());
+			return null;
+		}
+	}
+	
+	public static String chooseFile(String choosertitle, File workspacePath, String fileExtension) {
+		
+		String output = new String();
+		JFileChooser chooser;
+		chooser = new JFileChooser(); 
+		chooser.addChoosableFileFilter(new FileFilter() {
+			
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return fileExtension;
+			}
+			
+			@Override
+			public boolean accept(File file) {
+				// TODO Auto-generated method stub
+				if (file.getName().endsWith(fileExtension)||file.isDirectory()) {
+                    return true;
+                 }
+                 return false;
+			}
+		} );
+		if (workspacePath.getAbsolutePath() == "")
+			chooser.setCurrentDirectory(new java.io.File("."));
+		else
+			chooser.setCurrentDirectory(workspacePath);
+	    chooser.setDialogTitle(choosertitle);
+	    String temp = workspacePath.getPath();
+	    if (workspacePath.getPath().equals("0"))
+	    {
+	    	String chooserPath = null;
+	    	try
+	    	{
+	    		chooserPath = getPresetDirectoryPath(tempFilePath);
+	    		if (!chooserPath.equals(null))
+	    	    {
+	    			System.out.println("directory path ==== "+chooserPath);
+	    			chooser.setCurrentDirectory(new File(chooserPath));
+	    		}
+	    		else
+	    		{
+	    			System.out.println("Temp File Not Found");
+	    		}
+	    	}
+	    	catch(Exception e1)
+	    	{
+	    		System.out.println(e1.getMessage());
+	    		System.out.println("Error in creating the temp file");
+	    	}
+		    	
+	    }
+	    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	    chooser.setAcceptAllFileFilterUsed(false);
+	    if (chooser.showOpenDialog(chooser.getParent()) == JFileChooser.APPROVE_OPTION) { 
+			
+	        output = chooser.getSelectedFile().getAbsolutePath();
+	        return output;
+	        }
+	      else {
+	        System.out.println("No Selection ");
+	        return null;
+	        }
+	   }
+	
+public static String chooseFolder(String choosertitle, File workspacePath) {
+		
+		String output = new String();
+		JFileChooser chooser;
+		chooser = new JFileChooser(); 
+		
+		if (workspacePath.getAbsolutePath() == "")
+			chooser.setCurrentDirectory(new java.io.File("."));
+		else
+			chooser.setCurrentDirectory(workspacePath);
+	    chooser.setDialogTitle(choosertitle);
+	    String temp = workspacePath.getPath();
+	    if (workspacePath.getPath().equals("0"))
+	    {
+	    	String chooserPath = null;
+	    	try
+	    	{
+	    		chooserPath = getPresetDirectoryPath(tempFilePath);
+	    		if (!chooserPath.equals(null))
+	    	    {
+	    			System.out.println("directory path ==== "+chooserPath);
+	    			chooser.setCurrentDirectory(new File(chooserPath));
+	    		}
+	    		else
+	    		{
+	    			System.out.println("Temp File Not Found");
+	    		}
+	    	}
+	    	catch(Exception e1)
+	    	{
+	    		System.out.println(e1.getMessage());
+	    		System.out.println("Error in creating the temp file");
+	    	}
+		    	
+	    }
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    chooser.setAcceptAllFileFilterUsed(false);
+	    if (chooser.showOpenDialog(chooser.getParent()) == JFileChooser.APPROVE_OPTION) { 
+			
+	        output = chooser.getSelectedFile().getAbsolutePath();
+	        return output;
+	        }
+	      else {
+	        System.out.println("No Selection ");
+	        return null;
+	        }
+	   }
+
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 599, 596);
@@ -75,25 +227,25 @@ public class RegressionGUI {
 		targetLabel.setBounds(21, 43, 87, 16);
 		regressionSetting.add(targetLabel);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(120, 38, 130, 26);
-		regressionSetting.add(textField);
+		targetPathField = new JTextField();
+		targetPathField.setColumns(10);
+		targetPathField.setBounds(120, 38, 130, 26);
+		regressionSetting.add(targetPathField);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(412, 38, 130, 26);
-		regressionSetting.add(textField_1);
+		workspaceField = new JTextField();
+		workspaceField.setColumns(10);
+		workspaceField.setBounds(412, 38, 130, 26);
+		regressionSetting.add(workspaceField);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(120, 106, 130, 26);
-		regressionSetting.add(textField_2);
+		timeoutField = new JTextField();
+		timeoutField.setColumns(10);
+		timeoutField.setBounds(120, 106, 130, 26);
+		regressionSetting.add(timeoutField);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(412, 106, 130, 26);
-		regressionSetting.add(textField_3);
+		reportPathField = new JTextField();
+		reportPathField.setColumns(10);
+		reportPathField.setBounds(412, 106, 130, 26);
+		regressionSetting.add(reportPathField);
 		
 		timeoutLbl = new JLabel("Timeout in sec");
 		timeoutLbl.setHorizontalAlignment(SwingConstants.TRAILING);
@@ -111,14 +263,32 @@ public class RegressionGUI {
 		regressionSetting.add(lblReportpath);
 		
 		JButton targetBrowse = new JButton("Browse");
+		targetBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String path = chooseFolder("Target File Path", new File(""));
+				targetPathField.setText(path);
+			}
+		});
 		targetBrowse.setBounds(130, 67, 117, 29);
 		regressionSetting.add(targetBrowse);
 		
 		JButton workBrowse = new JButton("Browse");
+		workBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String path = chooseFolder("Workspace File Path", new File(""));
+				workspaceField.setText(path);
+			}
+		});
 		workBrowse.setBounds(425, 67, 117, 29);
 		regressionSetting.add(workBrowse);
 		
 		JButton reportBrowse = new JButton("Browse");
+		reportBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String path = chooseFolder("Report File Path", new File(""));
+				reportPathField.setText(path);
+			}
+		});
 		reportBrowse.setBounds(425, 136, 117, 29);
 		regressionSetting.add(reportBrowse);
 		
@@ -126,9 +296,9 @@ public class RegressionGUI {
 		start.setBounds(228, 186, 117, 29);
 		regressionSetting.add(start);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(21, 254, 523, 146);
-		regressionSetting.add(scrollPane);
+		JScrollPane console = new JScrollPane();
+		console.setBounds(21, 254, 523, 146);
+		regressionSetting.add(console);
 		JProgressBar progressBar=new JProgressBar();
 		progressBar.setBounds(6, 215, 554, 30);
         progressBar.setBorderPainted(true);
